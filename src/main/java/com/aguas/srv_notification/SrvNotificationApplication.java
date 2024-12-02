d1b1e61f391dc60211c104f31af846fed5d785ce
Daniel Pereira do Nascimento <daniel.nascimento@etipi.pi.gov.br>
2024-12-01 22:01:56 -0300

    fix: Carregando variáveis de ambiente

--- src/main/java/com/aguas/srv_notification/SrvNotificationApplication.java
+++ src/main/java/com/aguas/srv_notification/SrvNotificationApplication.java
@@ -1,13 +1,27 @@
 package com.aguas.srv_notification;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SrvNotificationApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SrvNotificationApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Iniciando aplicação...");
+		Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+            logger.debug("Carregando variável de ambiente: {}", entry.getKey());
+        });
+		logger.info("Iniciando Spring Application...");
 		SpringApplication.run(SrvNotificationApplication.class, args);
+		logger.info("Aplicação iniciada com sucesso!");
 	}
 
 }
